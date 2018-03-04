5f382862ebe4c9b9d4c1dfad3df30c2b1b13de72
Maliha Tarafdar <malihatarafdar@yahoo.com>
2018-03-03 23:57:11 -0500

    Add scroll to top

--- js/main.js
+++ js/main.js
@@ -29,3 +29,18 @@ function switchContent(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+/* Scroll to top button */
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
